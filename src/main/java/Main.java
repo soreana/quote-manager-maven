1d44acdeb3d55886f687ceb2e04c91667ac7ee0d
Sina Kashi Pazha <esterlinkof@gmail.com>
2017-11-17 19:15:22 +0330

    refactor Main class.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,6 +22,9 @@ public class Main {
     private static ArrayList<Facade> something = new ArrayList<>();
     private static Scanner console = new Scanner(System.in);
 
+    private static AtomicReference<Map<String, String>> userInput = new AtomicReference<>(new HashMap<>());
+    private static AtomicReference<ArrayList<Tag<String>>> tags = new AtomicReference<>(new ArrayList<Tag<String>>());
+
     static {
         try (InputStream input = new FileInputStream("src/main/resources/conf.properties")) {
 
@@ -44,66 +47,80 @@ public class Main {
                     .map(LambdaExceptionUtil.rethrowFunction(cl::loadClass))
                     .filter(e -> !e.isInterface())
                     .filter(Facade.class::isAssignableFrom)
-                    .forEach(LambdaExceptionUtil.rethrowConsumer(e->{
-                            Object obj = e.newInstance();
-                            Method method = e.getMethod("getInstance");
-                            Facade facade = (Facade) method.invoke(obj);
-                            something.add(facade);
-                            log.info("class " + e.getTypeName() + " was added as a facade.");
-                        }));
+                    .forEach(LambdaExceptionUtil.rethrowConsumer(e -> {
+                        Object obj = e.newInstance();
+                        Method method = e.getMethod("getInstance");
+                        Facade facade = (Facade) method.invoke(obj);
+                        something.add(facade);
+                        log.info("class " + e.getTypeName() + " was added as a facade.");
+                    }));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) throws MissedKeyException {
-        boolean quit = false;
-        boolean end;
-        AtomicReference<Map<String, String>> userInput = new AtomicReference<>(new HashMap<>());
-        AtomicReference<ArrayList<Tag<String>>> tags = new AtomicReference<>(new ArrayList<Tag<String>>());
-
-        while (!quit) {
+    private static Facade chooseFacade() {
+        while (true) {
             System.out.println("I can store these information for you:");
             for (int i = 0; i < something.size(); i++) {
                 System.out.println((i + 1) + ". " + something.get(i).getName());
             }
+
             System.out.println("q. quit");
-            System.out.print("please enter a number or q to quit:");
-            String input = console.next();
+            System.out.print("please enter a number or q and hit enter: ");
 
-            if (input.equals("q")) {
-                quit = true;
-                continue;
+            String input = console.nextLine();
+            if (input.equals("q"))
+                return null;
+            try {
+                return something.get(Integer.parseInt(input) - 1);
+            }catch (IndexOutOfBoundsException e){
+                System.err.println("invalid input, try again.");
+                System.err.flush();
+            }
+        }
+    }
+
+    private static void readStorageKeys(Facade f) {
+        Set<String> temp = f.getKeys();
+        String input;
+        userInput.get().clear();
+
+        for (String current : temp) {
+            System.out.print("please enter " + current + " : ");
+            input = console.nextLine();
+            userInput.get().put(current, input);
+        }
+    }
+
+    private static void readStorageTags() {
+        boolean end = false;
+        tags.get().clear();
+        do {
+            System.out.println("please enter tags comma separated, e.g: tag1, tag2, tag3");
+
+            for (String current : console.nextLine().replaceAll(" ", "").split(",")) {
+                if (!tags.get().contains(new Tag<>(current)))
+                    tags.get().add(new Tag<>(current));
             }
 
-            Facade f = something.get(Integer.parseInt(input) - 1);
-            Set<String> temp = f.getKeys();
-            userInput.get().clear();
+            System.out.println("add more tag [y/n]: ");
+            if (!"y".equals(console.nextLine()))
+                end = true;
+        } while (!end);
+    }
 
-            for (String current : temp) {
-                System.out.print("please enter " + current + " : ");
-                input = console.next();
-                userInput.get().put(current, input);
-            }
+    public static void main(String[] args) throws MissedKeyException {
+        Facade f = chooseFacade();
 
-            tags.get().clear();
-            end = false;
-            do {
-                System.out.println("please enter tags comma separated, e.g: tag1, tag2, tag3");
+        while (f!=null) {
+            readStorageKeys(f);
 
-                console.nextLine(); // to skip new lines
-
-                for (String current : console.nextLine().replaceAll(" ", "").split(",")) {
-                    if (!tags.get().contains(new Tag<>(current)))
-                        tags.get().add(new Tag<>(current));
-                }
-
-                System.out.println("add more tag [y/n]: ");
-                if (!"y".equals(console.next()))
-                    end = true;
-            } while (!end);
+            readStorageTags();
 
             f.addNewEntry(userInput.get(), tags.get());
+
+            f = chooseFacade();
         }
     }
 }
